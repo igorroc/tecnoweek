0ed6d8448d44d62746a139fc7bd0439929f99f7d
Igor Rocha <igor_roc@hotmail.com.br>
2022-05-08 18:18:52 -0300

    [add]: Icon component

---
+++ src/components/Icon.tsx
@@ -0,0 +1,18 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { IconPrefix, IconName } from "@fortawesome/fontawesome-svg-core"
+
+type Props = {
+	prefix: IconPrefix
+	iconName: IconName
+}
+
+export function Icon(props: Props) {
+	// const iconLookup: IconLookup = {
+	// 	prefix: props.prefix,
+	// 	iconName: props.iconName,
+	// }
+
+	// const iconDefinition: IconDefinition = findIconDefinition(iconLookup)
+
+	return <FontAwesomeIcon icon={[props.prefix, props.iconName]} />
+}
