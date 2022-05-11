4e608fb1a3cc8a2c40a463c8eef955ba46d2eb93
Igor Rocha <igor_roc@hotmail.com.br>
2022-05-11 14:15:53 -0300

    [add]: Editing form style

--- src/pages/Inscricao.tsx
+++ src/pages/Inscricao.tsx
@@ -6,6 +6,7 @@ const Modal = styled("div", {
 	"&": {
 		background: "#2F1E2D",
 		width: "500px",
+		paddingBottom: "20px",
 		display: "flex",
 		flexDirection: "column",
 		alignItems: "center",
@@ -34,6 +35,40 @@ const Modal = styled("div", {
 			display: "flex",
 			flexDirection: "column",
 			gap: "20px",
+			width: "70%",
+
+			"& .question": {
+				display: "flex",
+				flexDirection: "column",
+				gap: "10px",
+
+				"& input": {
+					background: "none",
+					border: "none",
+					borderBottom: "1px solid white",
+					outline: "none",
+					color: "white",
+				},
+			},
+
+			"& #timeline": {
+				display: "flex",
+				margin: "20px 0",
+
+				"& button": {
+					background: "#CA00B1",
+					color: "white",
+					fontWeight: "bold",
+					padding: "5px 10px",
+					border: "none",
+				},
+
+				"& #timelineBar": {
+					margin: "0 10px",
+					flex: "1",
+					background: "#F29DE8",
+				},
+			},
 		},
 	},
 })
@@ -50,25 +85,45 @@ function Inscricao() {
 				</div>
 				<h2>Inscreva-se na TecnoWeek</h2>
 				<form action="" id="form">
-					<div>
+					<div className="question">
 						<label htmlFor="nome">Nome Completo</label>
 						<input
 							type="text"
 							placeholder="Insira seu nome aqui..."
 						/>
 					</div>
-					<div>
-						<label htmlFor="nome">Matrícula</label>
-						<input type="text" placeholder="Ex: 202210111" />
+					<div className="question">
+						<label htmlFor="matricula">Matrícula</label>
+						<input
+							type="text"
+							id="matricula"
+							placeholder="Ex: 202210111"
+						/>
 					</div>
-					<div>
-						<label htmlFor="nome">Curso</label>
+					<div className="question">
+						<label htmlFor="curso">Curso</label>
 						<select name="curso" id="curso">
 							<option value="cic">Ciência da Computação</option>
 							<option value="fis">Física</option>
 							<option value="eng">Engenharia</option>
 							<option value="mat">Matemática</option>
 						</select>
+					</div>
+					<div className="question">
+						<label htmlFor="email">Email</label>
+						<input
+							type="email"
+							placeholder="Ex: pessoa@gmail.com"
+						/>
+					</div>
+					<div id="timeline">
+						<button>Anterior</button>
+						<div id="timelineBar">
+							{
+                                
+                            }
+						</div>
+						<button>Próximo</button>
 					</div>
 				</form>
 			</Modal>
